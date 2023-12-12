aee576864930c69bcbd6c8b80586c1b8f1fff4df
Aziah05 <s20049990@STUDENTS.DUVALSCHOOLS.ORG>
2023-12-12 14:10:32 -0500

    OOP progression

---
+++ .github/06_OOP/oop.py
@@ -0,0 +1,30 @@
+# Object-Oriented Programming, Aziah Hill, v0.0
+
+class Person: # Use PascalCase for ClassNames
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+   
+    # To string funion -- How the object appears as a string
+    def __str__(self):
+        return f"Name: {self.name}\nThis person is {self.age} years old. \nThey weigh {self.weight} pounds.\n"
+
+    def ClassFunction():
+        print("This is an example class funxtion.\n")
+        print("It only workswhen called on an object of that class.")
+
+person1 = Person("Gumball", 13, 124)
+person2 = Person("Darwin", 13, 116)
+print(person1)
+print(person2)
+
+if person1.weight > person2.weight:
+    print(f"{person1.name} weighs more than {person2.name}.")
+elif person1.weight == person2.weight:
+    print("Each person weighs the same.\n")
+else: 
+    print(f"{person2.name} weighs more than {person1.name}.")
+
+person1.classFunction()
+
---
+++ 5A_exampleGameFunctions/exampleGameFunctions.py
@@ -0,0 +1,20 @@
+# Example Game Functions Project, Hill Aziah, v0.0
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
+
+def catchBall(shootingPower, passCatchShoot, hand):
+    if shootingPower > 6.0 and passCatchShoot >= 10 and hand == 'Right':
+        ballCaught = True
+    elif hand == 'Left': 
+
