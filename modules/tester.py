fb7a77b0e2f43c762f35beb16a1a5bc53aeac795
Aziah05 <s20049990@STUDENTS.DUVALSCHOOLS.ORG>
2023-12-14 14:43:44 -0500

    tester update

--- modules/tester.py
+++ modules/tester.py
@@ -1,6 +1,19 @@
 import dice
+import perfmon
+start = perfmon.execStart()
 
 
-dice.roll(1,6)
-dice.display(1,6)
+
+
+roll1 = dice.roll(1,6)
+# roll2 = dice.display(1,6)
+
+print(roll1)
+if dice.isExploding(roll1, 6):
+    print("This roll exploded!\n")
+    roll1 += dice.roll(1, 6)
+    print(roll1)
+
+
+
 
