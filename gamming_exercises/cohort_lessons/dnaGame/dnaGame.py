2b69af8319fdb99596811c699beacf0ef0a8e5b1
Aziah05 <s20049990@STUDENTS.DUVALSCHOOLS.ORG>
2024-01-16 13:33:36 -0500

    Update dnaGame.py

--- gamming_exercises/cohort_lessons/dnaGame/dnaGame.py
+++ gamming_exercises/cohort_lessons/dnaGame/dnaGame.py
@@ -29,8 +29,8 @@ def genRNA(dnaSequence: str) -> tuple:
     print(f"The DNA Sequence is {dnaSequence}.\n")
     print("You will now geberate the RNA sequence that would match.\n")
     print("Please remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
-    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
     rnaStart =  time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan. 01, 1970
+    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
     rnaStop = time.time()
     rnaTime = rnaStop - rnaStart
     return (rnaSequence, rnaTime)
