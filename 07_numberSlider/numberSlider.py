04483074e3c4a9cde5396aead7638056c4f5444b
Aziah05 <s20049990@STUDENTS.DUVALSCHOOLS.ORG>
2024-01-22 14:04:50 -0500

    SLIDE

---
+++ 07_numberSlider/numberSlider.py
@@ -0,0 +1,25 @@
+
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e. Operating System Commands)
+
+from pygame.locals import *
+# Allows us to call functions from pygame using just the function name instead of modules.function()
+# Example: We can use draw() instead of pygame.draw()
+
+# Constants for Game Board
+BOARDWIDTH = 4 # COLUMNS
+BOARDHEIGHT = 4 # ROWS
+TILESIZE = 80 # WHAT UNIT DO YOU THINK THIS IS?
+WINDOWWIDTH = 640 # MEASURED IN PIXELS
+WINDOWHEIGHT = 480 # MEASURED IN PIXELS
+FPS = 30 # this is a maximmum value! Won't make a slow computer run faster.
+BLANK = None
+
+# COLOR CALUES in (R, G, B) format.
+# 0 = No Value, 225 = Max Value
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BRIGHTBLUE = (0, 50, 255)
+DARKTURQUOISE = (3, 54, 73)
+GREEN = (0, 204, 0)
