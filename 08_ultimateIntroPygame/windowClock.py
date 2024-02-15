06b3dfa9fb03d0f7e73e2482c1e28b3b30632395
Aziah <s20049990@students.duvalschools.org>
2024-02-15 14:48:38 -0500

    updated

--- 08_ultimateIntroPygame/windowClock.py
+++ 08_ultimateIntroPygame/windowClock.py
@@ -1,8 +1,6 @@
 import pygame
 from sys import exit
 
-# Please get caught up.  You are almost finished, you may need to do additional work outside of classtime. 
-
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
@@ -11,7 +9,10 @@ test_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load('img/ultimatePygame/Sky.png').convert()
 ground_surface = pygame.image.load('img/ultimatePygame/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surface = test_font.render('My game', False, 'Black')
+score_rect = score_surface.get_rect(center = (400,50))
+
 
 snail_surface = pygame.image.load('img/ultimatePygame/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600,300))
@@ -24,20 +25,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #   if player_rect.collidepoint(event.pos): print('collision')
     
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+    screen.blit(score_surface,score_rect)
     
     snail_rect.x -= 4
-    if snail_rect.right <= 0: snail_rect.left = 800
+    if snail_rect.right <= -100: snail_rect.left = 800
     screen.blit(snail_surface,snail_rect)
-    screen.blit(player_surface, player_rect)
+    screen.blit(player_surface,player_rect)
     
-    if player_rect.colliderect(snail_rect):
-        print('collision')
+    # if player_rect.colliderect(snail_rect):
+    #    print('collision')
     
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint((mouse_pos)):
+    #    print(pygame.mouse.get_pressed())
 
-
+    
     pygame.display.update()
     clock.tick(60)
