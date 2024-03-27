d742af401cf7671a624312e57d3ef4c34b0cb179
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-03-27 14:41:41 -0400

    Update finalProject.py

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -1,1 +1,8 @@
 # Final Project, Hill Aziah, v0.0
+import sys, random, pygame
+
+resolution = 0 # = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
+
+screen = pygame.display.set_mode((x, y))
+# CREAT AN if / else BLOCK TO SET RESOLUTION BASED ON THE VARIABLE ABOVE.
+
