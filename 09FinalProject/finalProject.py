bb691216437a0ada42b85f64106fd4721848f8e7
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-03-27 14:49:48 -0400

    Update finalProject.py

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -6,3 +6,20 @@ resolution = 0 # = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
 screen = pygame.display.set_mode((x, y))
 # CREAT AN if / else BLOCK TO SET RESOLUTION BASED ON THE VARIABLE ABOVE.
 
+if resolution == 0:
+    x = 800
+    y = 600
+else:
+    x = 1920
+    y = 1080
+
+pygame.init()
+
+difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
+
+if difficulty == 1:
+    pygame.display.set_caption('NAME OF GAME -- EASY')
+else:
+    pygame.display.set_caption('NAME OF GAME -- HARD')
+
+screen = pygame.display.set_mode((x, y))
