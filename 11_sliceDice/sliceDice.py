31d7c2eb8e48c0f4f538d28b6e7d5b1d9ffc96ca
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-03 14:07:44 -0400

    Create sliceDice.py

---
+++ 11_sliceDice/sliceDice.py
@@ -0,0 +1,24 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+myString = "Type a random string with punctuation, please.\n"
+
+print(alphabet[0])
+print(alphabet[-1])
+
+# Slicing
+print(alphabet[0:4]) # Slice UP TO second index.
+#  alphabet[start:stop]
+
+# Slice from the Start
+print(alphabet[:7])
+
+# Slice from the End
+print(alphabet[12:])
+
+# Slice from Whole %#$#!
+print(alphabet[:])
+
+# Negative Index Slicing
+print(alphabet[-4:-1])
+
+
+
