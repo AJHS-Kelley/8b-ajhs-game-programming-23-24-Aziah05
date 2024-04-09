d0132d99b68b10c13671601ae7081e73f7a98285
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-09 14:28:08 -0400

    Update finalProject.py

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -15,11 +15,12 @@ else:
 
 pygame.init()
 
-difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
-
-if difficulty == 1:
-    pygame.display.set_caption('NAME OF GAME -- EASY')
-else:
-    pygame.display.set_caption('NAME OF GAME -- HARD')
+def getDifficulty():
+    difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
+    if difficulty == 1:
+        pygame.display.set_caption('NAME OF GAME -- EASY')
+    else:
+        pygame.display.set_caption('NAME OF GAME -- HARD')
 
 screen = pygame.display.set_mode((x, y))
+
