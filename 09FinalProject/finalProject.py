7b7be899b2393d144b42e8d45ada0316a2b5ef01
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-11 14:46:35 -0400

    Update finalProject.py

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -6,10 +6,17 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
 # Be sure to put the images your using inside of the FIRST SET of parentheses
 sky_surface = pygame.image.load()()
 ground_surface = pygame.image.load()()
+
+score_surface = test_font.render('My game', False, (64,64,64))
+score_rect = score_surface.get_rect(center = (400,50))
+
+bird_surface = pygame.image.load('img/ultimatePygame/snail1.png').convert_alpha()
+bird_rect = bird_surface.get_rect(bottomright = (600,300))
 
 resolution = 0 # = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
 
