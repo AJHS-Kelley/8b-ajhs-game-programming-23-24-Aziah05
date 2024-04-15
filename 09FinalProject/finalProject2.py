068dd18a57391b3fb80314c9d03e0110b8693c09
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-15 14:27:16 -0400

    Create finalProject2.py

---
+++ 09FinalProject/finalProject2.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+import time
+import pygame
+
+screen = turtle.Screen()
+screen.title("SNAKE GAME")
+screen.setup(width = 800, height = 400)
+screen.tracer(0)
+screen.backgroundcolor("#1d1d1d")
+
+turtle.speed(5)
+turtle.pensize()
