64c901622d7ed784ac0539e4068b06b89b7ee542
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-15 14:43:42 -0400

    Update finalProject2.py

--- 09FinalProject/finalProject2.py
+++ 09FinalProject/finalProject2.py
@@ -10,4 +10,19 @@ screen.tracer(0)
 screen.backgroundcolor("#1d1d1d")
 
 turtle.speed(5)
-turtle.pensize()
+turtle.pensize(4)
+turtle.penup()
+turtle.goto(-310, 250)
+turtle.pendown()
+turtle.color("red")
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(600)
+turtle.right(90)
+
+
+
+
+turtle.hideturtle()
