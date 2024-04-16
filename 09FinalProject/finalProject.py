56c88299f8a428f04af3b4f20d7b5bf7beab59d4
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-16 19:32:00 -0400

    Progress made

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -1,40 +1,39 @@
-# Final Project, Hill Aziah, v0.0
+import turtle
+import random
+import time
 import pygame
-from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
-clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
 
-# Be sure to put the images your using inside of the FIRST SET of parentheses
-sky_surface = pygame.image.load()()
-ground_surface = pygame.image.load()()
+screen = turtle.Screen()
+screen.title("SNAKE GAME")
+screen.setup(width = 800, height = 400)
+screen.tracer(0)
+screen.backgroundcolor("#1d1d1d")
 
-score_surface = test_font.render('My game', False, (64,64,64))
-score_rect = score_surface.get_rect(center = (400,50))
+turtle.speed(5)
+turtle.pensize(4)
+turtle.penup()
+turtle.goto(-310, 250)
+turtle.pendown()
+turtle.color("red")
+turtle.forward(800)
+turtle.right(90)
+turtle.forward(800)
+turtle.right(90)
+turtle.forward(800)
+turtle.right(90)
+turtle.forward(800)
+turtle.right(90)
+turtle.forward(800)
+turtle.right(90)
+turtle.penup()
+turtle.hideturtle()
 
-bird_surface = pygame.image.load('img/ultimatePygame/snail1.png').convert_alpha()
-bird_rect = bird_surface.get_rect(bottomright = (600,300))
+# score
+score = 0
+delay = 0.1
 
-resolution = 0 # = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
-
-if resolution == 0:
-    x = 800
-    y = 600
-else:
-    x = 1920
-    y = 1080
-
-pygame.init()
-
-def getDifficulty():
-    difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
-    if difficulty == 1:
-        pygame.display.set_caption('NAME OF GAME -- EASY')
-    else:
-        pygame.display.set_caption('NAME OF GAME -- HARD')
-
-screen = pygame.display.set_mode((x, y))
-
+# snake
+snake = turtle.Turtle()
+snake.speed()
+snake.shape("square")
