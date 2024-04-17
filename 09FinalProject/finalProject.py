03c00e4be83f0e5c4c606bbdbcacc6d1c21d144c
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-17 19:49:49 -0400

    update

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -1,8 +1,17 @@
-import turtle
-import random
-import time
-import pygame
+import pygame as pg
+from random import randrange
 
+WINDOW = 800
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = Lambda; [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
+snake_dir = (0, 0)
+screen = pg.display.set_mode([WINDOW] * 2)
+clock = pg.time.Clock()
 
 screen = turtle.Screen()
 screen.title("SNAKE GAME")
@@ -37,3 +46,16 @@ delay = 0.1
 snake = turtle.Turtle()
 snake.speed()
 snake.shape("square")
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_d:
+                snake_dir - (0, TILE_SIZE)
