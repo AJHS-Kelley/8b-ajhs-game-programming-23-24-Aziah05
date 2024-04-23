6b034b8504473a2459982f251c26f6f0c49e0426
Aziah05 <142345393+Aziah05@users.noreply.github.com>
2024-04-23 11:58:40 -0400

    cancelled

--- 09FinalProject/finalProject.py
+++ 09FinalProject/finalProject.py
@@ -13,39 +13,6 @@ snake_dir = (0, 0)
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
-screen = turtle.Screen()
-screen.title("SNAKE GAME")
-screen.setup(width = 800, height = 400)
-screen.tracer(0)
-screen.backgroundcolor("#1d1d1d")
-
-turtle.speed(5)
-turtle.pensize(4)
-turtle.penup()
-turtle.goto(-310, 250)
-turtle.pendown()
-turtle.color("red")
-turtle.forward(800)
-turtle.right(90)
-turtle.forward(800)
-turtle.right(90)
-turtle.forward(800)
-turtle.right(90)
-turtle.forward(800)
-turtle.right(90)
-turtle.forward(800)
-turtle.right(90)
-turtle.penup()
-turtle.hideturtle()
-
-# score
-score = 0
-delay = 0.1
-
-# snake
-snake = turtle.Turtle()
-snake.speed()
-snake.shape("square")
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
